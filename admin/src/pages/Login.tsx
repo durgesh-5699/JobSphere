a7ba09a4e8a0f37aa2973686b2ebbf5d60ce382d
Durgesh Bhatt <durgeshbhatt360@gmail.com>
2026-08-17 01:02:24 +0530

    cors changes

--- admin/src/pages/Login.tsx
+++ admin/src/pages/Login.tsx
@@ -27,7 +27,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-     const response = await fetch(process.env.VITE_API_URL + "/api/admin/login", { 
+     const response = await fetch(import.meta.env.VITE_API_URL + "/api/admin/login", { 
   method: "POST",
   headers: {
     "Content-Type": "application/json",
